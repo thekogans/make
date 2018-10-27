ad64499c0c7188b9469317846c0d1880f442a9b6
Boris Kogan <boris@thekogans.net>
2018-10-26 22:37:26 -0700

    Rewrote get_toolchain_version command to handle multiple organizations/projects.

--- src/actions/get_toolchain_versions.cpp
+++ src/actions/get_toolchain_versions.cpp
@@ -16,7 +16,13 @@
 // along with thekogans_make. If not, see <http://www.gnu.org/licenses/>.
 
 #include <iostream>
-#include "thekogans/make/core/Toolchain.h"
+#include <set>
+#include <map>
+#include <iostream>
+#include "thekogans/util/Path.h"
+#include "thekogans/util/Version.h"
+#include "thekogans/util/Directory.h"
+#include "thekogans/make/core/Utils.h"
 #include "thekogans/make/Options.h"
 #include "thekogans/make/Action.h"
 
@@ -33,24 +39,64 @@ namespace thekogans {
 
                 virtual void PrintHelp (std::ostream &stream) const {
                     stream <<
-                        "-a:" << GetName () << " -o:organization -p:project\n\n"
+                        "-a:" << GetName () << " [-o:organization] [-p:project]\n\n"
                         "a - Get all installed versions for a given toolchain.\n"
-                        "o - Organization name.\n"
-                        "p - Project name.\n";
+                        "o - Optional organization name.\n"
+                        "p - Optional project name.\n";
                 }
 
                 virtual void Execute  () {
-                    std::list<std::string> versions;
-                    core::Toolchain::GetVersions (
-                        Options::Instance ().organization,
-                        Options::Instance ().project,
-                        versions);
-                    for (std::list<std::string>::const_iterator
-                             it = versions.begin (),
-                             end = versions.end (); it != end; ++it) {
-                        std::cout << *it << std::endl;
+                    std::string path =
+                        ToSystemPath (core::MakePath (core::_TOOLCHAIN_DIR, core::CONFIG_DIR));
+                    if (util::Path (path).Exists ()) {
+                        typedef std::pair<std::string, std::string> Toolchain;
+                        typedef std::set<util::Version, std::less<util::Version>> Versions;
+                        typedef std::map<Toolchain, Versions> ToolchainVersions;
+                        ToolchainVersions toolchainVersions;
+                        util::Directory directory (path);
+                        util::Directory::Entry entry;
+                        for (bool gotEntry = directory.GetFirstEntry (entry);
+                                gotEntry; gotEntry = directory.GetNextEntry (entry)) {
+                            if (entry.type == util::Directory::Entry::File) {
+                                std::string organization;
+                                std::string project;
+                                std::string branch;
+                                std::string version;
+                                std::string ext;
+                                if (core::ParseFileName (
+                                        entry.name,
+                                        organization,
+                                        project,
+                                        branch,
+                                        version,
+                                        ext) == 5 &&
+                                        (Options::Instance ().organization.empty () ||
+                                            Options::Instance ().organization == organization) &&
+                                        (Options::Instance ().project.empty () ||
+                                            Options::Instance ().project == project) &&
+                                        ext == core::XML_EXT) {
+                                    // Toolchain config files are branchless.
+                                    assert (branch.empty ());
+                                    toolchainVersions[Toolchain (organization, project)].insert (util::Version (version));
+                                }
+                            }
+                        }
+                        for (ToolchainVersions::const_iterator
+                                it = toolchainVersions.begin (),
+                                end = toolchainVersions.end (); it != end; ++it) {
+                            for (Versions::const_iterator
+                                    jt = it->second.begin (),
+                                    end = it->second.end (); jt != end; ++jt) {
+                                std::cout << core::GetFileName (
+                                    it->first.first,
+                                    it->first.second,
+                                    std::string (),
+                                    jt->ToString (),
+                                    core::XML_EXT) << std::endl;
+                            }
+                        }
+                        std::cout.flush ();
                     }
-                    std::cout.flush ();
                 }
             };
 
